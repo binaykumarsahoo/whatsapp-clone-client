b6ab8abb0845aa4f55a136e3a9e670929873f811
Binay Kumar Sahoo <binay.sahoo2013@gmail.com>
2021-06-12 21:39:13 +0530

    Updates

---
+++ precache-manifest.592cbef2b842da995423a45234cca43b.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecce03b3e542b1728acac053342374bd",
+    "url": "/whatsapp-clone-client/index.html"
+  },
+  {
+    "revision": "f51257157e28a9d530bc",
+    "url": "/whatsapp-clone-client/static/css/2.54c80412.chunk.css"
+  },
+  {
+    "revision": "6cbbd8798e14b8d55590",
+    "url": "/whatsapp-clone-client/static/css/main.69c584f8.chunk.css"
+  },
+  {
+    "revision": "f51257157e28a9d530bc",
+    "url": "/whatsapp-clone-client/static/js/2.c3001aea.chunk.js"
+  },
+  {
+    "revision": "802b7cd6755b5b1767da96ecaa2ad3b8",
+    "url": "/whatsapp-clone-client/static/js/2.c3001aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cbbd8798e14b8d55590",
+    "url": "/whatsapp-clone-client/static/js/main.6b60a142.chunk.js"
+  },
+  {
+    "revision": "5c35c75e3385a0689ccb",
+    "url": "/whatsapp-clone-client/static/js/runtime-main.9c556869.js"
+  },
+  {
+    "url": "/whatsapp-clone-client/static/media/bg-chat-tile-light_686b98c9fdffef3f63127759e2057750.686b98c9.png"
+  }
+]);
